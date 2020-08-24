7cb2a19752c8c47b345eb6f9dbf4e9f2668d0b6a
MaiQD <q.dat2710@gmail.com>
2020-08-24 13:47:45 +0700

    done version 1

---
+++ Face-Detection-App/Face-Detection-App/frmMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Face_Detection_App
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.cbxDevice = new System.Windows.Forms.ComboBox();
+			this.btnDetect = new System.Windows.Forms.Button();
+			this.picBox = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(45, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Thiết bị";
+			// 
+			// cbxDevice
+			// 
+			this.cbxDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbxDevice.FormattingEnabled = true;
+			this.cbxDevice.Location = new System.Drawing.Point(139, 22);
+			this.cbxDevice.Name = "cbxDevice";
+			this.cbxDevice.Size = new System.Drawing.Size(299, 24);
+			this.cbxDevice.TabIndex = 1;
+			// 
+			// btnDetect
+			// 
+			this.btnDetect.Location = new System.Drawing.Point(664, 66);
+			this.btnDetect.Name = "btnDetect";
+			this.btnDetect.Size = new System.Drawing.Size(124, 55);
+			this.btnDetect.TabIndex = 2;
+			this.btnDetect.Text = "Detect";
+			this.btnDetect.UseVisualStyleBackColor = true;
+			this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+			// 
+			// picBox
+			// 
+			this.picBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picBox.Location = new System.Drawing.Point(44, 66);
+			this.picBox.Name = "picBox";
+			this.picBox.Size = new System.Drawing.Size(614, 419);
+			this.picBox.TabIndex = 3;
+			this.picBox.TabStop = false;
+			this.picBox.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 540);
+			this.Controls.Add(this.picBox);
+			this.Controls.Add(this.btnDetect);
+			this.Controls.Add(this.cbxDevice);
+			this.Controls.Add(this.label1);
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Face Detection";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox cbxDevice;
+		private System.Windows.Forms.Button btnDetect;
+		private System.Windows.Forms.PictureBox picBox;
+	}
+}
+
---
+++ Face-Detection-App/Face-Detection-App/frmMain.cs
@@ -0,0 +1,76 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Face_Detection_App
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+		FilterInfoCollection filter;
+		VideoCaptureDevice device;
+		
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+			foreach (FilterInfo dev in filter)
+				cbxDevice.Items.Add(dev.Name);
+			if (cbxDevice.Items.Count>0)
+				cbxDevice.SelectedIndex = 0;
+			else
+				btnDetect.Enabled = false;
+			device = new VideoCaptureDevice();
+		}
+
+		private void btnDetect_Click(object sender, EventArgs e)
+		{
+			device = new VideoCaptureDevice(filter[cbxDevice.SelectedIndex].MonikerString);
+			device.NewFrame += Device_NewFrame;
+			device.Start();
+		}
+		static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("Training Data/haarcascade_frontalface_default.xml");
+		private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+		{
+			//ảnh gốc
+			Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+			Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+
+			Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+			foreach (var rectangle in rectangles)
+			{
+				using (Graphics graphics = Graphics.FromImage(bitmap))
+				{
+					using (Pen pen = new Pen(Color.Red,1))
+					{
+						graphics.DrawRectangle(pen, rectangle);
+					}
+				}
+			}
+			picBox.Image = bitmap;
+		}
+
+		private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (device.IsRunning)
+				device.Stop();
+		}
+	}
+}
